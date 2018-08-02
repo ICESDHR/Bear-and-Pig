a115db95dd91161c1945029af1071a871c3565ec
smilewangyizhe <smilewangyizhe@163.com>
2018-08-02 14:29:49 +0800

    排序算法

---
+++ Practice/Sort/HeapSort.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+# 本代码为建立小根堆
+
+def HeapAdjust(heap,length,i):
+    min = i
+    if 2*i+1 < length and heap[2*i+1] > heap[min]:
+        min = 2*i+1
+    if 2*i+2 < length and heap[2*i+2] > heap[min]:
+        min = 2*i+2
+    if min != i:
+        heap[i],heap[min] = heap[min],heap[i]
+        HeapAdjust(heap,length,min)
+
+
+def HeapSort(heap):
+    # 构建小根堆
+    for i in range(len(heap)//2-1,-1,-1):
+        HeapAdjust(heap,len(heap),i)
+    # 将根节点取出与最后一位做对调，对前面剩余节点继续进行对调整
+    for i in range(len(heap)-1,0,-1):
+        heap[0],heap[i] = heap[i],heap[0]
+        HeapAdjust(heap,i,0)
+    return heap
+
+if __name__ == '__main__':
+    a = [1, 7, 3, 9, 14, 2, 5, 9, 6, 10]
+    HeapSort(a)
+    print(a)
+
