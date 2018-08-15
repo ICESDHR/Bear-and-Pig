bde9e9a41e8e6db23fc61b22781b9aaab9641147
smilewangyizhe <smilewangyizhe@163.com>
2018-08-15 16:53:01 +0800

    剑指offer
    第14题

---
+++ Practice/Interview/Interview14.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+from functools import reduce
+
+# 剪绳子，已知绳子长度，将其剪为若干段，求能剪出的最大长度之积
+
+# 解法1：使用数学知识辅助，直接得到结果
+def CutRope1(n):
+	ans = [0]
+	for i in range(2,n+1):
+		average = n//i
+		if n%i == 0:
+			temp = [average for j in range(i)]
+		else:
+			temp = [average for j in range(i-n%i)] + [average+1 for j in range(n%i)]
+		ans.append(reduce(lambda a, b: a*b, temp))
+	return max(ans)
+
+# 解法2：使用动态规划
+def CutRope2(n):
+	ans = [0,1]
+	for i in range(2,n):
+		maxmium = 0
+		for j in range(i):
+			temp = max(ans[j],j+1)*max(ans[i-j-1],(i-j))
+			if temp > maxmium:
+				maxmium = temp
+		ans.append(maxmium)
+	return ans[-1]
+
+if __name__ == '__main__':
+	n = 13
+	print(CutRope1(n))
+	print(CutRope2(n))
